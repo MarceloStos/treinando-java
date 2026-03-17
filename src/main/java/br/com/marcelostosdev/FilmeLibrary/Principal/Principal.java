2aa255a8aa52d2c92a3e968f3a429c36be14272e
Marcelo <marcelostos.dev@gmail.com>
2026-03-16 21:48:18 -0300

    relembrando funções lambda

--- src/main/java/br/com/marcelostosdev/FilmeLibrary/Principal/Principal.java
+++ src/main/java/br/com/marcelostosdev/FilmeLibrary/Principal/Principal.java
@@ -3,12 +3,14 @@ package br.com.marcelostosdev.FilmeLibrary.Principal;
 import br.com.marcelostosdev.FilmeLibrary.Model.DadosEpisodio;
 import br.com.marcelostosdev.FilmeLibrary.Model.DadosSerie;
 import br.com.marcelostosdev.FilmeLibrary.Model.DadosTemporada;
+import br.com.marcelostosdev.FilmeLibrary.Model.Episodio;
 import br.com.marcelostosdev.FilmeLibrary.Service.ConsumoApi;
 import br.com.marcelostosdev.FilmeLibrary.Service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -33,9 +35,45 @@ public class Principal {
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
-//		temporadas.forEach(System.out::println);
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\n Top 10 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A) " + e))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .peek(e -> System.out.println("Ordenação " + e))
+                .limit(10)
+                .peek(e -> System.out.println("Limite " + e))
+                .map(e -> e.titulo().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento " + e))
+                .forEach(System.out::println);
+//        List<Episodio> episodios = temporadas.stream()
+//                .flatMap(t -> t.episodios().stream()
+//                    .map(e -> new Episodio(t.numero(), e))
+//                ).collect(Collectors.toList());
+//
+//        episodios.forEach(System.out::println);
+//
+//        System.out.println("A partir de que ano voce deseja ver os episódios? ");
+//        var ano = leitura.nextInt();
+//        leitura.nextLine();
+//
+//        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+//
+//        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        episodios.stream()
+//                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getTemporada() +
+//                                " Episódio: " + e.getNumeroEpisodio() +
+//                                " Data de lançamento: " + e.getDataLancamento().format(formatador)
+//                ));
     }
 }
